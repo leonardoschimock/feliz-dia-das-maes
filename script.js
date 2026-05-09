6fdb86ade6dc113391b28b3ae4a0ed711aaad5b5
Leonardo Schimock <dev.leonardosch@hotmail.com>
2026-05-08 23:53:55 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const card = document.getElementById("card");
+
+const openBtn = document.getElementById("openBtn");
+const backBtn = document.getElementById("backBtn");
+
+const typingText = document.getElementById("typingText");
+
+const message =
+`Mãe,
+
+obrigado por tudo que você faz todos os dias.
+
+Obrigado pelo carinho, pela força,
+pela paciência e por nunca desistir de mim.
+
+Você é meu porto seguro,
+minha inspiração
+e uma das pessoas mais importantes da minha vida.
+
+Eu te amo infinitamente. ❤️`;
+
+let i = 0;
+
+function typeWriter(){
+
+if(i < message.length){
+
+    typingText.innerHTML += message.charAt(i);
+
+    i++;
+
+    setTimeout(typeWriter, 35);
+}
+}
+
+openBtn.addEventListener("click", () => {
+
+card.classList.add("flip");
+
+typingText.innerHTML = "";
+i = 0;
+
+setTimeout(typeWriter, 600);
+
+});
+
+backBtn.addEventListener("click", () => {
+
+card.classList.remove("flip");
+
+});
